aebb6b4da41fa09bb46121c89c2d4c7e5cc679eb
Ji Li <liji15@mails.tsinghua.edu.cn>
2018-11-20 07:44:43 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,4 +3,9 @@
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board, index)
+  if board[index] == "" or board[index] == " " or board[index] == nil
+    return false
+  else board[index] == "X" or board[index] == "O"
+    return trues
+  end
 end
