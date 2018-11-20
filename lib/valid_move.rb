dd0c6e99103249f94685345d0fed9b45b98d0871
Ji Li <liji15@mails.tsinghua.edu.cn>
2018-11-20 07:48:25 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,5 +1,8 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
+  if index.between?(1, 9) and board == []
+    return true
+  end
   if index.between?(1, 9) and !position_taken(board, index)
     return true
   else
